567fc892713653b8996406f344ee41e7059c7156
Winston Bendana <winstonmartinbendana@gmail.com>
2023-10-02 01:15:53 +0800

    feat: added settings and server requirements to run with mariadb

--- license_portal/config/settings.py
+++ license_portal/config/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # https://docs.djangoproject.com/en/4.2/ref/databases/#connecting-to-the-database
+    # TODO: (prod) User env variables? 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'licenses',
+        'USER': 'licenses',
+        'PASSWORD': 'licenses',
+        'HOST': 'mariadb',
     }
 }
 
