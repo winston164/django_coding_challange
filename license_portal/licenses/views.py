5d831aeb717f0e19205a5beaa09df2f3fb49246b
Winston Bendana <winstonmartinbendana@gmail.com>
2023-10-02 19:35:28 +0800

    feat: simple NotifyRequest view to handle a POST request

--- license_portal/licenses/views.py
+++ license_portal/licenses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework import viewsets
+from .models import NotifyRequest
+from .serializers import NotifyRequestSerializer
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+
+class NotifyRequestViewSet(viewsets.ViewSet):
+    """ API endpoint to view notification
+    """
+    queryset = NotifyRequest.objects.all()
+    serializer = NotifyRequestSerializer
+
+    def create(self, request: Request):
+        serializer = self.serializer(data=request.data)
+        return Response(serializer.initial_data, status=status.HTTP_200_OK)
+
