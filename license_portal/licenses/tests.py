960f056e55942b9174808416ab9a64df458052e3
Winston Bendana <winstonmartinbendana@gmail.com>
2023-10-02 19:22:31 +0800

    test: posting notification request is reachable through POST

--- license_portal/licenses/tests.py
+++ license_portal/licenses/tests.py
@@ -5,11 +5,24 @@ from django.test import TestCase
 class NotFoundTest(TestCase):
     def test_404(self):
 
-        # Use non-existent view
+        # Prepare
         url = '/should-not-find'
 
-        # Assert response
+        # Execute
         response = self.client.get(url)
+
+        # Assert
         self.assertEqual(response.status_code, 404)
 
+class TestNotifications(TestCase):
+    def test_endpoint_reachable(self):
+
+        # Prepare
+        url = '/license-ms/notify/'
+
+        # Execute
+        response = self.client.post(url)
+
+        # Assert 
+        self.assertEqual(response.status_code, 200)
 
