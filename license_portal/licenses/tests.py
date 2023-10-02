f6c3674d1e855fc2617fce27f9f18cee100d2738
Winston Bendana <winstonmartinbendana@gmail.com>
2023-10-02 23:59:36 +0800

    test: request to notify returns no notifications

--- license_portal/licenses/tests.py
+++ license_portal/licenses/tests.py
@@ -1,4 +1,9 @@
+from datetime import datetime
 from django.test import TestCase
+from django.core.serializers import serialize
+
+from licenses.models import NotifyRequest
+from licenses.serializers import NotifyRequestSerializer
 
 # Create your tests here.
 
@@ -25,4 +30,26 @@ class TestNotifications(TestCase):
 
         # Assert 
         self.assertEqual(response.status_code, 200)
+
+    def test_no_notifications(self):
+        """ request with no objects in the database should only return request_datetime
+        """
+
+        # Prepare
+        url = '/license-ms/notify/'
+
+
+        # Execute
+        response = self.client.post(url)
+
+        # Assert 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(NotifyRequest.objects.count(), 1)
+
+        notify_request: NotifyRequest = NotifyRequest.objects.get()
+        notify_request = NotifyRequestSerializer(notify_request)
+
+        self.assertEqual(response.data, notify_request.data)
+
+
 
