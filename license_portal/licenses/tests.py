9f8cb3ce4500525b0ce14c6b89fbad572af6d8d4
Winston Bendana <winstonmartinbendana@gmail.com>
2023-10-06 18:16:58 +0800

    test: post notify request with populated notifications
    This test ensures that when the conditions are met to send a
    notification we receive a notify request response with the
    right schema.

--- license_portal/licenses/tests.py
+++ license_portal/licenses/tests.py
@@ -1,8 +1,16 @@
-from datetime import datetime
 from django.test import TestCase
-from django.core.serializers import serialize
+from django.utils import timezone
+from freezegun import freeze_time
 
-from licenses.models import NotifyRequest
+from licenses.models import (
+    Notification,
+    NotifyRequest,
+    License,
+    Client,
+    Package,
+    LicenseType
+)
+from django.contrib.auth.models import User
 from licenses.serializers import NotifyRequestSerializer
 
 # Create your tests here.
@@ -19,7 +27,8 @@ class NotFoundTest(TestCase):
         # Assert
         self.assertEqual(response.status_code, 404)
 
-class TestNotifications(TestCase):
+class TestNotificationsEndpiont(TestCase):
+
     def test_endpoint_reachable(self):
 
         # Prepare
@@ -31,6 +40,7 @@ class TestNotifications(TestCase):
         # Assert 
         self.assertEqual(response.status_code, 200)
 
+    @freeze_time('2023-01-01 12:00:00')
     def test_no_notifications(self):
         """ request with no objects in the database should only return request_datetime
         """
@@ -46,10 +56,70 @@ class TestNotifications(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(NotifyRequest.objects.count(), 1)
 
-        notify_request: NotifyRequest = NotifyRequest.objects.get()
-        notify_request = NotifyRequestSerializer(notify_request)
 
-        self.assertEqual(response.data, notify_request.data)
+        self.assertEqual(response.data, {
+            "request_datetime": '2023-01-01T12:00:00Z',
+            "notifications": []
+        })
+
+class TestCreateNotification(TestCase):
+
+    @freeze_time('2023-01-01 12:00:00')
+    def test_week_expiration(self):
+        """ 
+        Test to verify a notification resource is created
+        when a license expires in a week
+        """
+
+        # Prepare
+        user = User.objects.create_user(
+            username="user",
+            email="user@email.com",
+            password="password",
+        )
+        client = Client.objects.create(
+            client_name="client",
+            poc_contact_name = "Client Example",
+            poc_contact_email = "client@email.com",
+            admin_poc = user
+        )
+        lic = License.objects.create(
+            client=client,
+            package=Package.javascript_sdk,
+            license_type=LicenseType.production,
+            expiration_datetime=timezone.now() + timezone.timedelta(days=6)
+        )
+        url = '/license-ms/notify/'
+
+        # Execute
+        response = self.client.post(url)
+
+        # Assert 
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(NotifyRequest.objects.count(), 1)
+
+        self.assertEqual(Notification.objects.count(), 1)
+
+        self.assertEqual(response.data, {
+            "request_datetime": '2023-01-01T12:00:00Z',
+            "notifications": [
+                {
+                    "topic":  "Expiration Warning",
+                    "send_datetime": '2023-01-01T12:00:00Z',
+                    "message": "",
+                    "client_info": str(client),
+                    "admin_name": "user",
+                    'expiring_licenses': [
+                        {
+                            "type": "Production",
+                            "package": "JavaScript SDK",
+                            "expiration_date": "2023-01-07"
+                        }
+                    ]
+                }
+            ]
+        })
 
 
 
