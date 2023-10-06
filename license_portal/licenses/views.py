e3d73258ae345ebbb87b7c7aa18f31fa242ba2d9
Winston Bendana <winstonmartinbendana@gmail.com>
2023-10-06 18:25:42 +0800

    feat: basic notify request implementation
    Not complete, designed to pass previous test conditions.

--- license_portal/licenses/views.py
+++ license_portal/licenses/views.py
@@ -1,11 +1,18 @@
+from datetime import datetime
+import json
+from django.utils import timezone
 from rest_framework.request import Request
+from rest_framework.renderers import JSONRenderer
 from rest_framework import viewsets
-from .models import NotifyRequest
+from .models import License, Notification, NotificationTopic, NotifyRequest
 from .serializers import NotifyRequestSerializer
 
 from rest_framework import status
 from rest_framework.response import Response
 
+ONE_WEEK = timezone.timedelta(days=7)
+
+viewsets.ModelViewSet
 class NotifyRequestViewSet(viewsets.ViewSet):
     """ API endpoint to view notification
     """
@@ -13,8 +20,29 @@ class NotifyRequestViewSet(viewsets.ViewSet):
     serializer = NotifyRequestSerializer
 
     def create(self, request: Request):
-        serializer = self.serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        notify_req = NotifyRequest.objects.create()
+        date_now = timezone.now()
+
+        licenses: [License] = License.objects.all()
+
+        for client_license in licenses:
+            exp_date = client_license.expiration_datetime
+            exp_delta = exp_date - date_now
+            if exp_delta < ONE_WEEK:
+                message=""
+                notification = Notification.objects.create(
+                    topic=NotificationTopic.expiration_warning,
+                    client=client_license.client,
+                    message=message,
+                    user=client_license.client.admin_poc
+                )
+                notification.licenses.set([client_license])
+                notify_req.notifications.add(notification)
+                break
+
+
+        notify_req.save()
+
+        serialized_response = NotifyRequestSerializer(notify_req)
+        return Response(serialized_response.data, status=status.HTTP_200_OK)
 
