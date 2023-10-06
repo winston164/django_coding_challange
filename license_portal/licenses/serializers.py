b1076063df4ca55aaae561aec40b48426edb6315
Winston Bendana <winstonmartinbendana@gmail.com>
2023-10-06 18:23:31 +0800

    feat: added custom serializers for NotifyRequest and Notification

--- license_portal/licenses/serializers.py
+++ license_portal/licenses/serializers.py
@@ -1,10 +1,47 @@
-from .models import NotifyRequest
+from .models import Notification, NotifyRequest
 from rest_framework import serializers
 
+class NotificationSerializer(serializers.ModelSerializer):
+    client_info = serializers.SerializerMethodField()
+    expiring_licenses = serializers.SerializerMethodField()
+    admin_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Notification
+        fields = [
+            'topic',
+            'send_datetime',
+            'message',
+            'client_info',
+            'expiring_licenses',
+            'admin_name'
+        ]
+
+    def get_client_info(self, obj):
+        return str(obj.client)
+
+    def get_expiring_licenses(self, obj):
+        return [{
+            "type": lic.get_license_type_display(),
+            "package": lic.get_package_display(),
+            "expiration_date": lic.expiration_datetime.strftime('%Y-%m-%d')
+        } for lic in obj.licenses.all()]
+
+    def get_admin_name(self, obj):
+        return obj.user.username
+
 class NotifyRequestSerializer(serializers.ModelSerializer):
+    notifications = serializers.SerializerMethodField()
+
     class Meta:
         model = NotifyRequest
         fields = [
             'request_datetime',
             'notifications'
+        ]
+
+    def get_notifications(self, obj):
+        return [
+            NotificationSerializer(notification).data
+            for notification in obj.notifications.all()
         ]
